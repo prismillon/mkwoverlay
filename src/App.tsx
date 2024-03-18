3c1baa5b08c6e6c29260b005b4d20dff1a6c8c04
prismillon <phpoirier@gmail.com>
2024-03-18 21:23:16 +0100

    update style to match with mkwii and add mmr option

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,91 @@
 import React, { useEffect } from "react";
 import "./App.css";
-import { useSpring, animated } from "@react-spring/web";
+
+import g0 from "./char/g0.png";
+import g1 from "./char/g1.png";
+import g2 from "./char/g2.png";
+import g3 from "./char/g3.png";
+import g4 from "./char/g4.png";
+import g5 from "./char/g5.png";
+import g6 from "./char/g6.png";
+import g7 from "./char/g7.png";
+import g8 from "./char/g8.png";
+import g9 from "./char/g9.png";
+import gminus from "./char/gminus.png";
+import gplus from "./char/gplus.png";
+
+import r0 from "./char/r0.png";
+import r1 from "./char/r1.png";
+import r2 from "./char/r2.png";
+import r3 from "./char/r3.png";
+import r4 from "./char/r4.png";
+import r5 from "./char/r5.png";
+import r6 from "./char/r6.png";
+import r7 from "./char/r7.png";
+import r8 from "./char/r8.png";
+import r9 from "./char/r9.png";
+import rminus from "./char/rminus.png";
+import rplus from "./char/rplus.png";
+
+import y0 from "./char/y0.png";
+import y1 from "./char/y1.png";
+import y2 from "./char/y2.png";
+import y3 from "./char/y3.png";
+import y4 from "./char/y4.png";
+import y5 from "./char/y5.png";
+import y6 from "./char/y6.png";
+import y7 from "./char/y7.png";
+import y8 from "./char/y8.png";
+import y9 from "./char/y9.png";
+
+const CharMap = {
+  r0: r0,
+  r1: r1,
+  r2: r2,
+  r3: r3,
+  r4: r4,
+  r5: r5,
+  r6: r6,
+  r7: r7,
+  r8: r8,
+  r9: r9,
+  "r-": rminus,
+  "r+": rplus,
+  g0: g0,
+  g1: g1,
+  g2: g2,
+  g3: g3,
+  g4: g4,
+  g5: g5,
+  g6: g6,
+  g7: g7,
+  g8: g8,
+  g9: g9,
+  "g-": gminus,
+  "g+": gplus,
+  y0: y0,
+  y1: y1,
+  y2: y2,
+  y3: y3,
+  y4: y4,
+  y5: y5,
+  y6: y6,
+  y7: y7,
+  y8: y8,
+  y9: y9,
+};
 
 export default function App() {
   const searchParams = new URLSearchParams(window.location.search);
   const ladderType = searchParams.get("ct") === null ? "rt" : "ct";
+  const mmrType = searchParams.get("mmr") === null ? "lr" : "mmr";
+  const displayType = searchParams.get("mk8dx") === null ? "mkw" : "mk8dx";
   const name = searchParams.get("name");
 
   const [mmr, setMmr] = React.useState(0);
   const [rankURL, setRankURL] = React.useState("");
   const [diff, setDiff] = React.useState("");
   const [modClass, setModClass] = React.useState("");
-  const props = useSpring({ mmr: mmr });
 
   useEffect(() => {
     const fetchData = () =>
@@ -26,17 +100,32 @@ export default function App() {
           }
           data = data[0];
           console.log(data);
-          setMmr(data.updated_lr);
-          setRankURL(data.updated_emblem);
-          if (data.change_lr > 0) {
-            setModClass("modifier green");
-            setDiff("+" + data.change_lr);
-          } else if (data.change_lr < 0) {
-            setModClass("modifier red");
-            setDiff(data.change_lr);
-          } else {
-            setModClass("modifier disabled");
-            setDiff(data.change_lr);
+          if (mmrType === "lr") {
+            setMmr(data.updated_lr);
+            setRankURL(data.updated_emblem);
+            if (data.change_lr > 0) {
+              setModClass("modifier green");
+              setDiff("+" + data.change_lr);
+            } else if (data.change_lr < 0) {
+              setModClass("modifier red");
+              setDiff(data.change_lr);
+            } else {
+              setModClass("modifier disabled");
+              setDiff(data.change_lr);
+            }
+          } else if (mmrType === "mmr") {
+            setMmr(data.updated_mmr);
+            setRankURL(data.updated_emblem);
+            if (data.change_mmr > 0) {
+              setModClass("modifier green");
+              setDiff("+" + data.change_mmr);
+            } else if (data.change_mmr < 0) {
+              setModClass("modifier red");
+              setDiff(data.change_mmr);
+            } else {
+              setModClass("modifier disabled");
+              setDiff(data.change_mmr);
+            }
           }
         })
         .catch((error) => {
@@ -62,8 +151,8 @@ export default function App() {
   return (
     <div className="stats">
       <p className="wrapper">
-        <img src={rankURL} className="image" alt="" />
-        <animated.a>{props.mmr.to((x) => x.toFixed(0))}</animated.a>
+        <img src={rankURL} className="logo" alt="" />
+        <LrConverter mmr={mmr} />
         <Modifier modClass={modClass} modifier={diff} />
       </p>
     </div>
@@ -77,5 +166,45 @@ function Modifier({
   modClass: string;
   modifier: string;
 }) {
-  return <span className={modClass}>{modifier}</span>;
+  return (
+    <span className={modClass}>
+      <ModifierConverter modifier={modifier} />
+    </span>
+  );
+}
+
+function LrConverter({ mmr }: { mmr: number }) {
+  const value = mmr
+    .toString()
+    .split("")
+    .map((char, index) => {
+      return (
+        <img
+          key={index}
+          src={CharMap[("y" + char) as keyof typeof CharMap]}
+          alt={""}
+        />
+      );
+    });
+
+  return <a>{value}</a>;
+}
+
+function ModifierConverter({ modifier }: { modifier: string }) {
+  const sign = modifier.toString().split("")[0];
+  const mod = sign === "+" ? "g" : sign === "-" ? "r" : "y";
+  const value = modifier
+    .toString()
+    .split("")
+    .map((char, index) => {
+      return (
+        <img
+          key={index}
+          className="diff"
+          src={CharMap[(mod + char) as keyof typeof CharMap]}
+          alt={""}
+        />
+      );
+    });
+  return <a className="diffbox">{value}</a>;
 }
